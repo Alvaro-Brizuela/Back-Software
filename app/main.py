793e8183280259d50fac9a26128d43287793498d
Alvaro-Brizuela <alvarobrizuela01@gmail.com>
2025-09-29 07:27:54 -0300

    agregar dominio oxido a cors

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ origins = [
     "http://localhost:5173",  
     "http://127.0.0.1:5500",  
     "https://contapluss.netlify.app", # Producción real en Netlify
+    "https://oxido.cl", #Produccion temporal
 ]
 
 app.add_middleware(
